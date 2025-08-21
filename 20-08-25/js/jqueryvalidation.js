52993ee007579362c3100d5958aa27843beb046b
xDaBix <21bmiit091@gmail.com>
2025-08-21 14:50:57 +0530

    21-08-25

---
+++ 20-08-25/js/jqueryvalidation.js
@@ -0,0 +1,195 @@
+$(document).ready(function(){
+    $('#username').on('input',validateName);
+    $('#mobileno').on('input',validateMobileno);
+    $('#email').on('input',validateEmail);
+    $('#gender').on('change',validateGender);
+    $('#country').on('change',validateCountry);
+    $('#dob').on('change',validateDob);
+    $('#password').on('input',validatePwd);
+    $('#cpassword').on('input',validateCpwd);
+    $('#agree').on('change',validateTerms);
+    
+    
+    function validateName(){
+        const $name=$('#username').val().trim();
+        const $namepattern=/^[a-zA-Z]+$/;
+        if($name=='' || $name==null){
+            $("#NameErr").html("please enter the name")
+            return false;
+
+        }else if(!$namepattern.test($name)){
+            $("#NameErr").html("please enter the valid name")
+            return false;
+        }
+        else{
+            $("#NameErr").html("")
+            return true;
+
+        }
+    }
+    
+    function validateMobileno(){
+        const $mobileno=$('#mobileno').val().trim();
+        if($mobileno=='' || $mobileno==null){
+            $("#mobilenoErr").html("please enter the mobile no");
+            return false;
+
+        }else if($mobileno.length != 10){
+            $("#mobilenoErr").html("mobile no should be 10 digits");
+            return false;
+        }
+        else{
+            $("#mobilenoErr").html("");
+            return true;
+
+        }
+    }
+
+    function validateEmail(){
+        const $email=$('#email').val().trim();
+        const $emailpattern=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if($email==''||$email==null){
+            $("#emailErr").html("please enter email ids");
+            return false;
+        
+        }else if(!$emailpattern.test($email)){
+            $("#emailErr").html("please enter the valid email id")
+            return false;
+        }
+        else{
+            $("#emailErr").html("")
+            return true;
+
+        }
+    }
+
+    function validateGender(){
+        const $genderMale=$('#GenderMale');
+        const $genderFemale=$('#GenderFemale');
+        if(!$genderMale.checked && !$genderFemale.checked){
+
+            $('#genderErr').html("please select the genders");
+            return false;
+        }
+
+        else{
+            $('#genderErr').html("");
+            return true;
+        }
+
+    }
+
+    function validateDob(){
+        const $dob=$('#dob').val().trim();
+        if(!$dob){
+            $("#dobErr").html("please select date of birth");
+            return false;
+        
+        }
+        else{
+            $("#dobErr").html("");
+            return true;
+
+
+        }
+
+
+
+    }
+
+    function validatePwd(){
+        const $pwd=$('#password').val().trim();
+        
+        if($pwd==''||$pwd==null){
+            $("#pwdErr").html("please enter password");
+            return false;
+        
+        }else if($pwd.length<6){
+            $("#pwdErr").html("password must be 6 characters")
+            return false;
+        }
+        else{
+            $("#pwdErr").html("");
+            return true;
+
+
+        }
+    }
+
+    function validateCpwd(){
+        const $cpwd=$('#cpassword').val().trim();
+        
+        if($cpwd==''||$cpwd==null){
+            $("#cpwdErr").html("please enter password");
+            return false;
+        
+        }else if($pwd!=$cpwd){
+            $("#cpwdErr").html("password must be same");
+            return false;
+        }
+        else{
+            $("#cpwdErr").html("");
+            return false;
+
+
+        }
+    }
+
+    function validateTerms(){
+        const $terms=$('#agree');
+        if(!$terms.checked){
+            $('#agreeErr').html("please select the genders");
+            return false;
+        }
+
+        else{
+            $('#agreeErr').html("");
+            return true;
+        }
+
+    }
+
+    function validateCountry() {
+            const $country = document.getElementById("country").value.trim();
+            if ($country=='Country') {
+            $('#countryErr').html("please select the country");
+                return false;
+            } else {
+            $('#countryErr').html("");
+
+                
+                return true;
+            }
+        }
+
+
+
+ $("#signupform").on('submit',function(e){
+    
+            e.preventDefault();
+
+            const $isValidUser = validateName();
+            const $isValidGender = validateGender();
+            const $isValidDOB = validateDob();
+            const $isValidCountry = validateCountry();
+            const $isValidMobile = validateMobileno();
+            const $isValidEmail = validateEmail();
+            const $isValidPwd = validatePwd();
+            const $isValidTerms = validateTerms();
+            const $isValidCpwd = validateCpwd();
+
+
+            
+            if( $isValidUser && $isValidGender && $isValidDOB && $isValidCountry && $isValidMobile && $isValidEmail && $isValidPwd && $isValidTerms && $isValidCpwd){
+                return true;
+
+            }
+            else{
+                return false;
+            }
+            
+            
+        })
+    
+
+})
